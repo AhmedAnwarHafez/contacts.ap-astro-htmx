1e493d2a0983528ab7147cafdc14a124ca631bfe
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-07-13 18:43:21 +1200

    pretty

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,49 +1,50 @@
-import db from "./connection";
+import db from './connection'
 
 export interface ContactDraft {
-  name: string;
-  email: string;
-  phone: string;
+  name: string
+  email: string
+  phone: string
 }
 
 interface Contact extends ContactDraft {
-  id: number;
+  id: number
 }
 
 export function getTotalContacts() {
-  return db("contacts")
-    .count("* as total")
-    .then(([{ total }]) => total);
+  return db('contacts')
+    .count('* as total')
+    .then(([{ total }]) => total)
 }
 
 export function getAllContacts() {
-  return db("contacts")
-    .select("id", "name", "email", "phone", "created_at")
-    .orderBy("created_at", "desc");
+  return db('contacts')
+    .select('id', 'name', 'email', 'phone', 'created_at')
+    .orderBy('created_at', 'desc')
 }
 
 export async function getContactById(id: number) {
-  return await db("contacts")
-    .where("id", id)
-    .first("id", "name", "email", "phone");
+  return await db('contacts')
+    .where('id', id)
+    .first('id', 'name', 'email', 'phone')
 }
 
 export async function addContact(contact: ContactDraft) {
-  return await db("contacts").insert(contact);
+  return await db('contacts').insert(contact)
 }
 
 export async function deleteContact(id: number) {
-  return await db("contacts").where("id", id).del();
+  return await db('contacts').where('id', id).del()
 }
 
 export async function updateContact(contact: Contact) {
-  return await db("contacts").where("id", contact.id).update(contact);
+  console.log(contact)
+  return await db('contacts').where('id', contact.id).update(contact)
 }
 
 export async function searchContacts(query: string) {
-  return await db("contacts")
-    .whereLike("name", `%${query}%`)
-    .orWhereLike("email", `%${query}%`)
-    .orWhereLike("phone", `%${query}%`)
-    .orderBy("created_at", "desc");
+  return await db('contacts')
+    .whereLike('name', `%${query}%`)
+    .orWhereLike('email', `%${query}%`)
+    .orWhereLike('phone', `%${query}%`)
+    .orderBy('created_at', 'desc')
 }
